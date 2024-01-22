3e91b23070b50c81ca2eef797d00c947772bb49d
Ogbaje Leo Arome <leonardaromeh@gmail.com>
2024-01-22 10:51:48 +0100

    Delete Functionality added :fire:

--- src/components/RenderTodos.vue
+++ src/components/RenderTodos.vue
@@ -20,7 +20,7 @@
                 </svg>
                 </button>
                   
-                <button class='pl-4' onclick="deleteTodo('${todo.uuid}')" title="Delete">
+                <button class='pl-4' v-on:click="deleteTodo(todo.id)" title="Delete">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="red" class="w-6 h-6">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
               </svg>
@@ -32,6 +32,8 @@
 </template>
 
 <script>
+import Swal from 'sweetalert2'
+
 export default {
     name:'RenderTodos',
     props:{
@@ -40,7 +42,27 @@ export default {
     data(){return{
             todos:Array
     }},
-    mounted(){
+    methods:{
+    deleteTodo(id){
+    Swal.fire({
+    title: "Are you sure?",
+    text: "You won't be able to revert this!",
+    icon: "warning",
+    showCancelButton: true,
+    confirmButtonColor: "#3085d6",
+    cancelButtonColor: "#d33",
+    confirmButtonText: "Yes, delete it!",
+  }).then((result) => {
+    if (result.isConfirmed) {
+      const filteredDB =(JSON.parse(localStorage.getItem('DB') )|| []) .filter((todo) => todo.id !== id);
+      localStorage.setItem('DB', JSON.stringify(filteredDB))
+     
+
+      Swal.fire("Deleted!", "Your Todo has been deleted.", "success");
+    }
+  });
+}},
+    mounted() {
         this.todos=(JSON.parse(localStorage.getItem('DB') )|| [])
         this.todos=this.todos.sort((a, b) => {
     if (a.created_on > b.created_on) return -1
@@ -49,6 +71,7 @@ export default {
 
     }) 
 },
+
 }
 </script>
 
